1dca4c844e808c23a550c496b05558d74c54e680
Aalekh Patel <aalekh.gwpeck.7998@icloud.com>
2024-08-18 21:23:59 -0500

    Add solution for broadcast part 2
    Signed-off-by: Aalekh Patel <aalekh.gwpeck.7998@icloud.com>

--- solutions/src/bin/broadcast.rs
+++ solutions/src/bin/broadcast.rs
@@ -1,13 +1,14 @@
 use serde::{Serialize, Deserialize};
-use solutions::{message::Envelope, io::io_channel};
+use solutions::{io::io_channel, message::{Body, Envelope}};
 use tokio::sync::mpsc::UnboundedSender;
 use tracing_subscriber::EnvFilter;
-use std::{collections::HashMap, sync::atomic::{AtomicUsize, Ordering}};
+use std::{collections::HashMap, sync::atomic::{AtomicUsize, Ordering}, time::Duration};
+use std::sync::{Arc, Mutex};
 
 
 static MSG_ID: AtomicUsize = AtomicUsize::new(1);
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Clone, Serialize, Deserialize)]
 #[serde(tag = "type", rename_all = "snake_case")]
 #[non_exhaustive]
 pub enum Payload {
@@ -27,7 +28,10 @@ pub enum Payload {
     Topology {
         topology: HashMap<String, Vec<String>>
     },
-    TopologyOk
+    TopologyOk,
+    Gossip {
+        messages: Vec<usize>,
+    }
 }
 
 fn message_id() -> usize {
@@ -39,18 +43,27 @@ pub struct State {
     my_id: String,
     topology: HashMap<String, Vec<String>>,
     seen_messages: Vec<usize>,
+    buffered_messages: Vec<usize>,
+}
+
+
+impl State {
+    pub fn neighbors(&self) -> impl Iterator<Item=&String> + '_ {
+        // Assume we can talk to everyone rn.
+        self.topology.keys().filter(|&key| key != &self.my_id)
+    }
 }
 
 
 #[tracing::instrument(skip(writer))]
 pub async fn handle_envelope(
-    state: &mut State,
+    state: Arc<Mutex<State>>,
     envelope: Envelope<Payload>, 
     writer: UnboundedSender<Envelope<Payload>>
 ) {
     match &envelope.body.message {
         Payload::Init { node_id, .. } => {
-
+            let mut state = state.lock().unwrap();
             state.my_id = node_id.clone();
 
             let reply = envelope.reply_with(
@@ -60,6 +73,7 @@ pub async fn handle_envelope(
             writer.send(reply).unwrap();
         },
         Payload::Topology { topology } => {
+            let mut state = state.lock().unwrap();
             state.topology = topology.clone();
             let reply = envelope.reply_with(
                 Some(message_id()),
@@ -68,7 +82,9 @@ pub async fn handle_envelope(
             writer.send(reply).unwrap();
         },
         Payload::Broadcast { message } => {
+            let mut state = state.lock().unwrap();
             state.seen_messages.push(*message);
+            state.buffered_messages.push(*message);
             let reply = envelope.reply_with(
                 Some(message_id()),
                 Payload::BroadcastOk
@@ -76,22 +92,62 @@ pub async fn handle_envelope(
             writer.send(reply).unwrap();
         },
         Payload::Read => {
+            let state = state.lock().unwrap();
             let reply = envelope.reply_with(
                 Some(message_id()),
                 Payload::ReadOk { messages: state.seen_messages.clone() }
             );
             writer.send(reply).unwrap();
-        }
+        },
+        Payload::Gossip { messages } => {
+            let mut state = state.lock().unwrap();
+            state.seen_messages.extend(messages);
+            // let reply = envelope.reply_with(
+            //     Some(message_id()),
+            //     Payload::ReadOk { messages: state.seen_messages.clone() }
+            // );
+            // writer.send(reply).unwrap();
+        },
 
         _ => {}
     }
 }
 
+
+#[tracing::instrument(skip(writer))]
+pub async fn gossip_every_so_often(
+    state: Arc<Mutex<State>>, 
+    writer: UnboundedSender<Envelope<Payload>>
+) {
+    let mut interval = tokio::time::interval(Duration::from_millis(500));
+    interval.tick().await;
+
+    loop {
+        interval.tick().await;
+        {
+            let mut state = state.lock().unwrap();
+            let payload = Payload::Gossip { messages: state.buffered_messages.clone() };
+            let body = Body { msg_id: Some(message_id()), in_reply_to: None, message: payload };
+            for neighbor in state.neighbors() {
+                let envelope = Envelope::new(&state.my_id, neighbor, body.clone());
+                writer.send(envelope).unwrap();
+            }
+            state.buffered_messages.clear();
+        }    
+    }
+}
+
 pub async fn server() {
-    let mut state = State::default();
+    let state = Arc::new(Mutex::new(State::default()));
     let (writer, mut reader, _) = io_channel::<Envelope<Payload>>();
+
+    let state_cp = state.clone();
+    let writer_cp = writer.clone();
+
+    tokio::task::spawn(gossip_every_so_often(state_cp, writer_cp));
+
     while let Some(envelope) = reader.recv().await {
-        handle_envelope(&mut state, envelope, writer.clone()).await;
+        handle_envelope(state.clone(), envelope, writer.clone()).await;
     }
 }
 
